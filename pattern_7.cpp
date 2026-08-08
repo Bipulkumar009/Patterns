a6c10f3e07f1cc22a9e5d1a7068537f03acd7071
Bipul Kumar <bipulkumar009vk@gmail.com>
2026-08-08 11:21:14 +0530

    Pattren 7

---
+++ pattern_7.cpp
@@ -0,0 +1,24 @@
+/*
+Pattern 7
+        *
+      * * *
+    * * * * * 
+  * * * * * * *
+* * * * * * * * * 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            cout<<"  ";
+        }
+        for(int j=0;j<=2*i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
