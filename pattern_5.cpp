3f2b4b95db27ef6d10d98000bedfc823ac016460
Bipul Kumar <bipulkumar009vk@gmail.com>
2026-08-08 10:25:28 +0530

    Pattern 5

---
+++ pattern_5.cpp
@@ -0,0 +1,21 @@
+/*
+Pattern 5
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
