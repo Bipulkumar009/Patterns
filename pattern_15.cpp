080329e74194c95b068208410ffb078c6cb97f25
Bipul Kumar <bipulkumar009vk@gmail.com>
2026-08-09 13:54:25 +0530

    Pattern 15

---
+++ pattern_15.cpp
@@ -0,0 +1,21 @@
+/*
+Pattern 15
+A B C D E
+A B C D
+A B C 
+A B 
+A
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<(n-i);j++){
+            cout<<char('A'+j)<<" ";
+        }
+        cout<<endl;
+    }
+}
