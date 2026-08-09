7104a5ecaff5bed4ab14b25df11ed0a32ca3fa0a
Bipul Kumar <bipulkumar009vk@gmail.com>
2026-08-09 13:20:24 +0530

    Pattern 11

---
+++ pattern_11.cpp
@@ -0,0 +1,22 @@
+/*
+Pattern 11
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<(i+j)%2<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
